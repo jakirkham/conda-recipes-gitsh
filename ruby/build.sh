3c37c7ed8a4adab2256761b248be2d029526a15f
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-21 14:01:40 -0400

    ruby/build.sh: Make sure no docs are built, ruby's shared library is built, and using relative paths works.

--- ruby/build.sh
+++ ruby/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash configure --prefix=$PREFIX --disable-install-doc --enable-load-relative
+bash configure --prefix=$PREFIX --enable-load-relative --enable-shared --disable-install-doc --disable-install-rdoc
 make
 make install
 
