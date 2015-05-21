89fd39e582fcbb9dd148a1c5245029490aecd945
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-21 15:00:02 -0400

    ruby/build.sh: Make sure the right dependencies are used.

--- ruby/build.sh
+++ ruby/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash configure --prefix=$PREFIX --enable-load-relative --enable-shared --disable-install-doc --disable-install-rdoc
+bash configure --prefix=$PREFIX --enable-load-relative --enable-shared --disable-install-doc --disable-install-rdoc --with-openssl-dir=$PREFIX --with-readline-dir=$PREFIX --with-tcl-dir=$PREFIX --with-tk-dir=$PREFIX --with-libyaml-dir=$PREFIX --with-zlib-dir=$PREFIX
 make
 make install
 
